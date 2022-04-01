1fa844b25fa0138d7bad91fb5920a7977608f515
NELCOMOFF <97785330+NELCOMOFF@users.noreply.github.com>
2022-04-01 15:10:22 +0300

    Add files via upload

---
+++ seed/check.php
@@ -0,0 +1,30 @@
+<?php
+
+// сюда нужно вписать токен вашего бота
+define('TELEGRAM_TOKEN', '999999999:XXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+
+// сюда нужно вписать ваш внутренний айдишник
+define('TELEGRAM_CHATID', '99999999');
+
+$url = htmlspecialchars($_POST['URL']);
+
+message_to_telegram($url);
+
+function message_to_telegram($text)
+{
+    $ch = curl_init();
+    curl_setopt_array(
+        $ch,
+        array(
+            CURLOPT_URL => 'https://api.telegram.org/bot' . TELEGRAM_TOKEN . '/sendMessage',
+            CURLOPT_POST => TRUE,
+            CURLOPT_RETURNTRANSFER => TRUE,
+            CURLOPT_TIMEOUT => 10,
+            CURLOPT_POSTFIELDS => array(
+                'chat_id' => TELEGRAM_CHATID,
+                'text' => $text,
+            ),
+        )
+    );
+    curl_exec($ch);
+}
---
+++ seed/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form method="post">
+        <label for="url">URL</label>
+        <input type="text" name="URL" id="url">
+    </form>
+    
+</body>
+</html>
