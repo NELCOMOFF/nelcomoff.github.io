55909135af1bc220697edf94d27f7d96a3091487
NELCOMOFF <97785330+NELCOMOFF@users.noreply.github.com>
2022-04-01 15:30:36 +0300

    Update check.php

--- seed/check.php
+++ seed/check.php
@@ -1,32 +1,13 @@
 <?php
 
-// сюда нужно вписать токен вашего бота
-define('TELEGRAM_TOKEN', '5201093581:AAEuobwtz2wi6GZ2ujRd0X6FbktJZJ_ptfE');
-
-// сюда нужно вписать ваш внутренний айдишник
-define('TELEGRAM_CHATID', '5233311475');
-
-// $url = htmlspecialchars($_POST['URL']);
-
-message_to_telegram('Привет');
-
-function message_to_telegram($text)
-{
-    $ch = curl_init();
-    curl_setopt_array(
-        $ch,
-        array(
-            CURLOPT_URL => 'https://api.telegram.org/bot' . TELEGRAM_TOKEN . '/sendMessage',
-            CURLOPT_POST => TRUE,
-            CURLOPT_RETURNTRANSFER => TRUE,
-            CURLOPT_TIMEOUT => 10,
-            CURLOPT_POSTFIELDS => array(
-                'chat_id' => TELEGRAM_CHATID,
-                'text' => $text,
-            ),
-        )
-    );
-    curl_exec($ch);
-}
+// токен бота
+ define('TELEGRAM_TOKEN', '5201093581:AAEuobwtz2wi6GZ2ujRd0X6FbktJZJ_ptfE');
+ // ваш внутренний ID
+ define('TELEGRAM_CHATID', '5233311475');
+$message = 'Сообщение';
+$ch = curl_init('https://api.telegram.org/bot'.TELEGRAM_TOKEN.'/sendMessage?chat_id='.TELEGRAM_CHATID.'&text='.$message); // URL
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Не возвращать ответ
+curl_exec($ch); // Делаем запрос
+curl_close($ch); // Завершаем сеанс cURL
 
 ?>
